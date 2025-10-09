ae9eb252664c3bae6781fc43c1b3617a6e907dd4
FIstNCISBall <lulubraun1999@gmail.com>
2025-10-09 20:25:38 +0000

    kam wieder nimm halt die andere DB wieder

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-5131389322-5af0a",
-  "appId": "1:446572448299:web:f7282b50d18d40d9d43b17",
-  "storageBucket": "studio-5131389322-5af0a.appspot.com",
+  "projectId": "studio-1025620015-a7beb",
+  "appId": "1:446572448299:web:355e9488736e6e69123849",
+  "storageBucket": "studio-1025620015-a7beb.appspot.com",
   "apiKey": "AIzaSyC2qjA-2Hnh2i42Pt4s-A8b_DSprl7j4uM",
-  "authDomain": "studio-5131389322-5af0a.firebaseapp.com",
+  "authDomain": "studio-1025620015-a7beb.firebaseapp.com",
   "messagingSenderId": "446572448299"
 };
