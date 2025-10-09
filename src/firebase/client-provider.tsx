4d23046b76c564c4ff713cc2cf187451696d2e02
FIstNCISBall <lulubraun1999@gmail.com>
2025-10-09 21:29:38 +0000

    das kam

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useMemo, type ReactNode } from 'react';
+import React, { type ReactNode } from 'react';
 import { FirebaseProvider } from '@/firebase/provider';
 import { firebaseApp, auth, firestore } from '@/firebase';
 
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -31,14 +31,6 @@ export { app as firebaseApp, auth, firestore };
 //   };
 // }
 
-export function getSdks(firebaseApp: FirebaseApp) {
-  return {
-    firebaseApp,
-    auth: getAuth(firebaseApp),
-    firestore: getFirestore(firebaseApp),
-  };
-}
-
 export * from './provider';
 export * from './client-provider';
 export * from './firestore/use-collection';
