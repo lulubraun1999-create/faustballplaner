fa77637e4f8308916519aaf760010752bdad263b
FIstNCISBall <lulubraun1999@gmail.com>
2025-10-09 22:20:11 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,6 +57,10 @@ module.exports = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      fontFamily: {
+        body: ["Inter", "sans-serif"],
+        headline: ["Inter", "sans-serif"],
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -66,12 +70,23 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "fade-in": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "fade-in": "fade-in 0.5s ease-out forwards",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
